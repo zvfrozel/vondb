bc3e844f59d9f3f7ec1d1460e23901f7b89e381c
zvfrozel <zvfrozel@gmail.com>
2025-03-23 13:50:41 +0000

    better handling of von_tmp

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -7,16 +7,19 @@
   ];
 
   env = {
-    # Set XDG_CONFIG_HOME to your project's .config folder
     XDG_CONFIG_HOME = "/home/user/vondb/.config";
+    TMPDIR = "/home/user/vondb/tmp";
     PATH = [ "/home/user/vondb/.venv/bin" ];
   };
 
   idx = {
     workspace = {
       onStart = {
-        # Create the virtual environment if it doesn't exist and install vondb
         create-venv-and-install-vondb = ''
+          # Create TMP directory if it doesn't exist
+          mkdir -p /home/user/vondb/tmp
+
+          # Create the virtual environment if it doesn't exist and install vondb
           if [ ! -d .venv ]; then
             python -m venv .venv
           fi
